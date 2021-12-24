7b8cc9e95e36cdb368f2ab6001ad73d58546b460
nguyenkien0798 <kiennguyen0798@gmail.com>
2021-12-24 23:23:47 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const moment = require("moment");
+
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+
+const middlewares = jsonServer.defaults();
+
+server.db = router.db;
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = moment().valueOf();
+    req.body.updatedAt = moment().valueOf();
+  }
+
+  if (req.method === "PUT") {
+    req.method = "PATCH";
+  }
+
+  if (req.method === "PATCH") {
+    req.body.updatedAt = moment().valueOf();
+  }
+
+  next();
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.use(auth);
+server.use(router);
+server.listen(PORT);
