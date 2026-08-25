b2613d0881845b2c0b3e57e12cd7af75b1fb56f2
Kien Nguyen Dinh <kiennguyen0798@gmail.com>
2026-08-25 16:03:13 +0700

    Update code

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const auth = require("json-server-auth");
 const moment = require("moment");
 
 const server = jsonServer.create();
-const router = jsonServer.router("./db/db.json");
+const router = jsonServer.router("./db.json");
 
 const middlewares = jsonServer.defaults();
 
